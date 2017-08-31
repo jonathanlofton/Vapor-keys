e458ff58719749e7b4e97c17cd6ce22b692c2126
Jonathan M. Lofton <jonathanlofton91@gmail.com>
2017-08-31 13:44:09 -0700

    Updated

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,9 +1,45 @@
 import React from 'react';
 
 const App = () => (
-  <div>
-    <p>React is working</p>
-  </div>
+    <div class="title-keyboard">
+      <h1 class="head-title">Vapor Keys</h1>
+      <div class="keyboard-wrapper">
+        <div class="keyboard">
+          <div class="top-row">
+            <div data-key="81" class="key">Tom 1</div>
+            <div data-key="87" class="key">Tom 2</div>
+            <div data-key="69" class="key">Tom 3</div>
+            <div data-key="82" class="key">Hat 1</div>
+            <div data-key="84" class="key">Hat 2</div>
+            <div data-key="89" class="key">Hat 3</div>
+            <div data-key="85" class="key">Snare 4</div>
+            <div data-key="73" class="key">Snare 5</div>
+            <div data-key="79" class="key">Snare 6</div>
+            <div data-key="80" class="key">Snare 7</div>
+          </div>
+          <div class="middle-row">
+            <div data-key="65" class="key">Snare 1</div>
+            <div data-key="83" class="key">Snare 2</div>
+            <div data-key="68" class="key">Snare 3</div>
+            <div data-key="70" class="key">Crash</div>
+            <div data-key="71" class="key">Clap</div>
+            <div data-key="72" class="key">Snap</div>
+            <div data-key="74" class="key">Bass 1</div>
+            <div data-key="75" class="key">Bass 2</div>
+            <div data-key="76" class="key">Bass 3</div>
+          </div>
+          <div class="bottom-row">
+            <div data-key="90" class="key">Grunt</div>
+            <div data-key="88" class="key">Boop 1</div>
+            <div data-key="67" class="key">Boop 2</div>
+            <div data-key="86" class="key">Boop 3</div>
+            <div data-key="66" class="key">Boop 4</div>
+            <div data-key="78" class="key">Boop 5</div>
+            <div data-key="77" class="key">Clap 2</div>
+          </div>
+        </div>
+      </div>
+    </div>
 );
 
 export default App;
